ad2783924eec977d3eebd7cad611af66dce55b4a
Christoffer Nordgren <christoffernordgren981009@gmail.com>
2024-11-19 08:48:35 +0100

    uppdateringgit add .

--- Assets/Scripts/KlotScript.cs
+++ Assets/Scripts/KlotScript.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UIElements;
 
 public class KlotScript : MonoBehaviour
 {
     public GameObject Player;
     public float movementSpeed;
+    [SerializeField] Transform target;
+    NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
         transform.position = new Vector3(Random.Range(-8, 8), Random.Range(-8, 8), Random.Range(-8, 8));
     }
 
     // Update is called once per frame
     void Update()
     {
+        agent.SetDestination(target.position);
         transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, Time.deltaTime * movementSpeed);
     }
 
